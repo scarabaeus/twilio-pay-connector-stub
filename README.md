429697fb79460b13271171b8016b92d6484669ee
Steve Gomez <StevenAGomez@Live.com>
2024-02-01 14:58:22 -0800

    Updates to initial

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # twilio-pay-connector-stub
 
 This is a stub for the Twilio Pay Connector. It is a simple web server that listens for incoming requests and responds with a JSON payload.
+
+## Test Card
+
+Card Number: `4111 1111 1111 1111`
+Expiry Date: `12/25`
+Billing Zip: `94105`
+CVV: `333`
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
 import express from 'express';
+import { uuid } from 'uuidv4';
 const app = express();
 
+app.use(express.json());
+
+const respond = (req, res) => {
+  console.dir(
+    `==================== Payment Info Received via ${
+      req.method
+    } at ${new Date().toLocaleString('en-US')} ====================`,
+    { depth: null }
+  );
+  console.dir(req.body, { depth: null });
+
+  const chargeResponse = {
+    confirmationNumber: uuid(),
+    error_code: null,
+    error_message: null,
+  };
+
+  console.log(`==================== Response ====================`);
+  console.dir(chargeResponse, { depth: null });
+
+  res.json(chargeResponse);
+};
+
 app.get('/', function (req, res) {
-  res.send('twilio-pay-connector-stub');
+  respond(req, res);
+});
+
+app.post('/', function (req, res) {
+  respond(req, res);
+});
+
+app.put('/', function (req, res) {
+  respond(req, res);
+});
+
+app.patch('/', function (req, res) {
+  respond(req, res);
 });
 
 app.listen(8888);
